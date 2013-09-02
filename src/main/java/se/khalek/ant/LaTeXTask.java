8a071bdb4f1ef3b8fa5d8a7053daaafd64451e18
khalek <fredrik.stil@gmail.com>
2013-09-02 15:26:11 +0200

    Added the initial logs for the program. To tests logs, run the new build.xml in the test dir.

--- src/main/java/se/khalek/ant/LaTeXTask.java
+++ src/main/java/se/khalek/ant/LaTeXTask.java
@@ -1,23 +1,87 @@
 package se.khalek.ant;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 /**
  * Ant task for compiling LaTeX documents. The task uses pdfTeX as compiler.
- *
  */
 public class LaTeXTask extends Task {
-		String source;
+		// Attributes of the task. //
+		private String source;
+		private String workingDir = System.getProperty("user.dir");
+		private boolean clean;
 
+		/**
+		 * Sets the required attribute for filename to source latex file.
+		 * 
+		 * @param src The filename of the source latex file.
+		 */
 		public void setSource(String src) {
 			source = src;
 		}
+		
+		/**
+		 * Sets the working directory for pdfTeX.
+		 * If this attribute is not set then the current directory will
+		 * be chosen.
+		 * 
+		 * @param dir Working directory.
+		 */
+		public void setWorkingDir(String dir) {
+			workingDir = dir;
+		}
+		
+		/**
+		 * Sets the value of the attribute clean.
+		 * 
+		 * @param doClean Assign true if files, such as .log and .aux, 
+		 * should be removed after compilation, false otherwise.
+		 */
+		public void setClean(boolean doClean) {
+			clean = doClean;
+		}
+		
+		/**
+		 * Retrieves the current application version from
+		 * the version.properties resource.
+		 * 
+		 * @return The version number.
+		 */
+		private String version() {
+			String version = "";
+			URL resource = getClass().getResource("/version.properties");
+			Properties props = new Properties();
+			try {
+				props.load(resource.openStream());
+				version = props.getProperty("version");
+			} catch (IOException e) {
+				version = "undetermined";
+				e.printStackTrace();
+			}
+			return version;
+		}
 
+		/**
+		 * Executes this and any nested tasks.
+		 * 
+		 * @throws BuildException if an error occurs during execution.
+		 */
 		public void execute() throws BuildException {
+			// Make sure that source file is a required attribute.
 			if (source == null) {
 					throw new BuildException("No latex source file was given.");
 			}
-			log(source);
+						
+			log("Executing LaTeX ANT Task, Version " + version());
+			
+			// Log the values of all attributes.
+			log("source \t = " + source);
+			log("workingDir \t = " + workingDir);
+			log("clean \t = " + clean);
 		}
 }
