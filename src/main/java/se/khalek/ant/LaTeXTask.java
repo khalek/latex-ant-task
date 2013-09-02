385337f4897145d43b16363efbf741e799b0bb53
khalek <fredrik.stil@gmail.com>
2013-09-02 22:36:28 +0200

    LaTeXTask.java. Refactored the functionality for converting strings into a File object.

--- src/main/java/se/khalek/ant/LaTeXTask.java
+++ src/main/java/se/khalek/ant/LaTeXTask.java
@@ -13,96 +13,124 @@ import org.apache.tools.ant.types.Path;
  * Ant task for compiling LaTeX documents. The task uses pdfTeX as compiler.
  */
 public class LaTeXTask extends Task {
-		// Attributes of the task. //
-		private String source;
-		private String workingDir = System.getProperty("user.dir");
-		private boolean clean;
+	// Attributes of the task. //
+	private String source;
+	private String workingDir = System.getProperty("user.dir");
+	private boolean clean;
 
-		/**
-		 * Sets the required attribute for filename to source latex file.
-		 * 
-		 * @param src The filename of the source latex file.
-		 */
-		public void setSource(String src) {
-			source = src;
+	/**
+	 * Sets the required attribute for filename to source latex file.
+	 * 
+	 * @param src
+	 *            The filename of the source latex file.
+	 */
+	public void setSource(String src) {
+		source = src;
+	}
+
+	/**
+	 * Sets the working directory for pdfTeX. If this attribute is not set then
+	 * the current directory will be chosen.
+	 * 
+	 * @param dir
+	 *            Working directory.
+	 */
+	public void setWorkingDir(String dir) {
+		workingDir = dir;
+	}
+
+	/**
+	 * Sets the value of the attribute clean.
+	 * 
+	 * @param doClean
+	 *            Assign true if files, such as .log and .aux, should be removed
+	 *            after compilation, false otherwise.
+	 */
+	public void setClean(boolean doClean) {
+		clean = doClean;
+	}
+
+	/**
+	 * Retrieves the current application version from the version.properties
+	 * resource.
+	 * 
+	 * @return The version number.
+	 */
+	private String version() {
+		String version = "";
+		URL resource = getClass().getResource("/version.properties");
+		Properties props = new Properties();
+		try {
+			props.load(resource.openStream());
+			version = props.getProperty("version");
+		} catch (IOException e) {
+			version = "undetermined";
 		}
-		
-		/**
-		 * Sets the working directory for pdfTeX.
-		 * If this attribute is not set then the current directory will
-		 * be chosen.
-		 * 
-		 * @param dir Working directory.
-		 */
-		public void setWorkingDir(String dir) {
-			workingDir = dir;
+		return version;
+	}
+
+	/**
+	 * Converts the String workingDir to a canonical version.
+	 * 
+	 * @return The canonical path to the working directory.
+	 * @throws BuildException
+	 *             If an error occurs when retrieving the path.
+	 */
+	private String canonical() throws BuildException {
+		File directory = new File(workingDir);
+		try {
+			return directory.getCanonicalPath();
+		} catch (IOException e) {
+			throw new BuildException(e.getMessage());
 		}
-		
-		/**
-		 * Sets the value of the attribute clean.
-		 * 
-		 * @param doClean Assign true if files, such as .log and .aux, 
-		 * should be removed after compilation, false otherwise.
-		 */
-		public void setClean(boolean doClean) {
-			clean = doClean;
+	}
+
+	/**
+	 * Creates and return a file object based on two parameters. The first
+	 * parameter is the directory to the file and the second, the name of the
+	 * file.
+	 * 
+	 * @param dir
+	 *            Path to the files directory.
+	 * @param file
+	 *            The name of the file.
+	 * @return A File object for the file.
+	 * @throws BuildException
+	 *             If the file does not exist.
+	 */
+	private File convertToFile(String dir, String file) throws BuildException {
+		String canonicalPath = dir + "/" + file;
+		File f = new File(canonicalPath);
+		if (!f.exists()) {
+			throw new BuildException("File " + canonicalPath
+					+ " does not exist.");
 		}
-		
-		/**
-		 * Retrieves the current application version from
-		 * the version.properties resource.
-		 * 
-		 * @return The version number.
-		 */
-		private String version() {
-			String version = "";
-			URL resource = getClass().getResource("/version.properties");
-			Properties props = new Properties();
-			try {
-				props.load(resource.openStream());
-				version = props.getProperty("version");
-			} catch (IOException e) {
-				version = "undetermined";
-				e.printStackTrace();
-			}
-			return version;
-		}
-		
-		private String canonical() throws BuildException {
-			File directory = new File(workingDir);
-			try {
-				return directory.getCanonicalPath();
-			} catch (IOException e) {
-				// Print stack trace only during development
-				e.printStackTrace();
-				throw new BuildException(e.getMessage());				
-			}
+		return f;
+	}
+
+	/**
+	 * Executes this and any nested tasks.
+	 * 
+	 * @throws BuildException
+	 *             if an error occurs during execution.
+	 */
+	public void execute() throws BuildException {
+		// Make sure that a source file is given as an attribute.
+		if (source == null) {
+			throw new BuildException("No latex source file was given.");
 		}
 
-		/**
-		 * Executes this and any nested tasks.
-		 * 
-		 * @throws BuildException if an error occurs during execution.
-		 */
-		public void execute() throws BuildException {
-			// Make sure that source file is a required attribute.
-			if (source == null) {
-				throw new BuildException("No latex source file was given.");
-			}
-			
-			workingDir = canonical();
-			
-			String canonicalPath = workingDir + "/" + source;
-			File file = new File(canonicalPath);
-			if (! file.exists()) {
-				throw new BuildException("File " + canonicalPath + " does not exist.");
-			}
-						
-			log("Executing LaTeX ANT Task, Version " + version());
-			
-			// Log the values of all attributes.
-			log("source \t = " + source);
-			log("workingDir \t = " + workingDir);
-			log("clean \t = " + clean);
-		}
+		// Convert working directory path to canonical.
+		workingDir = canonical();
+
+		// Convert the file name and its path to a File object.
+		File sourceFile = convertToFile(workingDir, source);
+		
+		log("Executing LaTeX ANT Task, Version " + version());
+
+		// Log the values of all attributes.
+		log("source \t = " + source);
+		log("workingDir \t = " + workingDir);
+		log("clean \t = " + clean);
+	}
 }
